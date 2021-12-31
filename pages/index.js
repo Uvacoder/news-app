5aadf5839b42b70fc7feca8e4d1e786dfcdc9f1c
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-12-31 03:56:31 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,53 +19,53 @@ export default function Home({ newsList }) {
   );
 }
 
-export async function getStaticProps() {
-  try {
-    const newsList = await fetch(`
-https://newsapi.org/v2/everything?q=tesla&from=2021-11-02&sortBy=publishedAt&sources?country=us&language=en&apiKey=${NEWS_API_KEY}`).then(
-      (res) => res.json()
-    );
+//export async function getStaticProps() {
+//  try {
+//    const newsList = await fetch(`
+//https://newsapi.org/v2/everything?q=tesla&from=2021-11-02&sortBy=publishedAt&sources?country=us&language=en&apiKey=${NEWS_API_KEY}`).then(
+//      (res) => res.json()
+//    );
     //     const newsList = await fetch(`
     // https://newsapi.org/v2/everything?q=tesla&from=2021-11-02&sortBy=publishedAt&apiKey=${NEWS_API_KEY}`).then(
     //       (res) => res.json()
     //     );
     // console.log(newsList);
-    return {
-      props: {
-        newsList,
-      },
-    };
-  } catch (error) {
-    console.error(error.message);
-  }
-
+  //  return {
+  //    props: {
+  //      newsList,
+ //     },
+ //   };
+ // } catch (error) {
+//    console.error(error.message);
+ // }
+//
   // By returning { props: { posts } }, the Blog component
   // will receive `posts` as a prop at build time
-  return {
-    props: {
-      NewsList: [],
-    },
-  };
-}
+ // return {
+ //   props: {
+ //     NewsList: [],
+ //   },
+ // };
+//}
 
-// export async function getServerSideProps() {
-//   try {
-//     const newsList = await fetch(`
-// https://newsapi.org/v2/everything?q=tesla&from=2021-11-02&sortBy=publishedAt&apiKey=${NEWS_API_KEY}`).then(
-//       (res) => res.json()
-//     );
-//     return {
-//       props: {
-//         newsList,
-//       },
-//     };
-//   } catch (error) {
-//     console.error(error.message);
-//   }
+ export async function getServerSideProps() {
+   try {
+     const newsList = await fetch(`
+       https://newsapi.org/v2/everything?q=tesla&from=2021-11-02&sortBy=publishedAt&apiKey=${NEWS_API_KEY}`).then(
+       (res) => res.json()
+      );
+    return {
+       props: {
+         newsList,
+       },
+     };
+   } catch (error) {
+     console.error(error.message);
+   }
 
-//   return {
-//     props: {
-//       NewsList: [],
-//     },
-//   };
-// }
+   return {
+     props: {
+       NewsList: [],
+     },
+   };
+  }
